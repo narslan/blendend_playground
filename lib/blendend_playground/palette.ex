08e93ddf69ac457f2b31ba775179e6fc1f932634
Nevroz Arslan <newrozarslan@gmail.com>
2025-11-26 11:19:56 +0300

    [refactor] Add color palettes

--- lib/blendend_playground/palette.ex
+++ lib/blendend_playground/palette.ex
@@ -10,8 +10,6 @@ defmodule BlendendPlayground.Palette do
 
   # defstruct [:name, :colors, :stroke, :background]
 
-  # Palette set adapted from takawo (https://openprocessing.org/user/6533) —
-  # used here to ease multicolor experimentation.
   @scheme_palette %{
     benedictus: ["#F27EA9", "#366CD9", "#5EADF2", "#636E73", "#F2E6D8"],
     cross: ["#D962AF", "#58A6A6", "#8AA66F", "#F29F05", "#F26D6D"],
@@ -19,9 +17,11 @@ defmodule BlendendPlayground.Palette do
     hiroshige: ["#1B618C", "#55CCD9", "#F2BC57", "#F2DAAC", "#F24949"],
     hokusai: ["#074A59", "#F2C166", "#F28241", "#F26B5E", "#F2F2F2"],
     hokusai_blue: ["#023059", "#459DBF", "#87BF60", "#D9D16A", "#F2F2F2"],
+    hokusai3: ["#d8d97a", "#95c36e", "#74c8c3", "#5a97c1", "#295384", "#0a2e57"],
     java: ["#632973", "#02734A", "#F25C05", "#F29188", "#F2E0DF"],
     kandinsky: ["#8D95A6", "#0A7360", "#F28705", "#D98825", "#F2F2F2"],
     monet: ["#4146A6", "#063573", "#5EC8F2", "#8C4E03", "#D98A29"],
+    klimt: ["#df9ed4", "#c93f55", "#eacc62", "#469d76", "#3c4b99", "#924099"],
     nizami: ["#034AA6", "#72B6F2", "#73BFB1", "#F2A30F", "#F26F63"],
     renoir: ["#303E8C", "#F2AE2E", "#F28705", "#D91414", "#F2F2F2"],
     vangogh: ["#424D8C", "#84A9BF", "#C1D9CE", "#F2B705", "#F25C05"],
