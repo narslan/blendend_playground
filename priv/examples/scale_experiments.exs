b1225b046b90f3109079a76d8882976652dd51ae
Nevroz Arslan <newrozarslan@gmail.com>
2025-12-15 00:35:30 +0300

    feat: Add single colors from d3.js

--- priv/examples/scale_experiments.exs
+++ priv/examples/scale_experiments.exs
@@ -3,46 +3,31 @@
 alias BlendendPlayground.Palette
 
 draw 800, 800 do
-  palette =
-    Palette.palette_by_name("takamo.VanGogh")
-    |> Map.get(:colors, [])
-    |> Palette.from_hex_list_rgb()
+#https://observablehq.com/@d3/colorbrewer-splines
+palette = ["#8e0152", "#c51b7d", "#de77ae", "#f1b6da", "#fde0ef", "#f7f7f7", "#e6f5d0", "#b8e186", "#7fbc41", "#4d9221", "#276419"]
+palette = Palette.from_hex_list_rgb(palette)
 
-  scale =
-    Scale.Ordinal.new(
-      domain: ["a", "b", "c", "d", "e"],
-      range: palette
-    )
-
-  col = Scale.map(scale, "a")
-  IO.inspect(col)
-
-  clear(fill: rgb(col))
-
-  scale =
-    Scale.Linear.new(
+  scale2 =
+    Scale.Quantize.new(
       domain: [0, 1],
-      range: [{165, 42, 42}, {70, 130, 180}],
-      interpolate: &Scale.Interpolator.rgb/2
-    )
+      range: palette)
 
-  stops =
-    for i <- 0..4 do
-      t = i / 4
+  stops2 =
+    for i <- 0..10 do
+      t = i / 10
       # => {t, {r,g,b}}
-      {t, Scale.map(scale, t)}
+      {t, Scale.map(scale2, t)}
     end
 
-  grad =
-    linear_gradient 40, 40, 420, 420 do
-      for {t, {r, g, b}} <- stops do
+  grad2 =
+    linear_gradient 40, 40, 760, 100 do
+      for {t, {r, g, b}} <- stops2 do
         add_stop(t, rgb(r, g, b))
       end
     end
 
-  translate(200, 200)
-  round_rect(40, 40, 420, 420, 28, 28, fill: grad)
-
-  font = load_font("priv/fonts/Alegreya-Regular.otf", 48.0)
-  text(font, 80, 215, "Hello, blendend!", fill: rgb(40, 40, 40))
+  #translate(200, 200)
+  box(40, 40, 760, 100, fill: grad2)
+  
+  
 end
