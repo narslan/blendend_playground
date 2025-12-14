91f9adf997110d643a8b109acf4417c66c05b4ba
Nevroz Arslan <newrozarslan@gmail.com>
2025-12-14 05:08:12 +0300

    Add warning

--- priv/examples/scale_experiments.exs
+++ priv/examples/scale_experiments.exs
@@ -1,40 +1,42 @@
-
+# This is here to experiment a private repo.
+# It will fail for now.
 alias BlendendPlayground.Palette
-use Blendend.Draw
-draw 800, 800 do
-  
-  palette =
-  Palette.palette_by_name("takamo.VanGogh")
-  |> Map.get(:colors, [])
-  |> Palette.from_hex_list_rgb()
-  
 
- scale = Scale.Ordinal.new(
-        domain: ["a", "b", "c", "d", "e"],
-        range: palette)
- 
+draw 800, 800 do
+  palette =
+    Palette.palette_by_name("takamo.VanGogh")
+    |> Map.get(:colors, [])
+    |> Palette.from_hex_list_rgb()
+
+  scale =
+    Scale.Ordinal.new(
+      domain: ["a", "b", "c", "d", "e"],
+      range: palette
+    )
+
   col = Scale.map(scale, "a")
   IO.inspect(col)
 
-  
   clear(fill: rgb(col))
-  
- scale = Scale.Linear.new(
-        domain: [0, 1],
-        range: [{165, 42, 42}, {70, 130, 180}],
-        interpolate: &Scale.Interpolator.rgb/2
-      )
-    
-   stops =
+
+  scale =
+    Scale.Linear.new(
+      domain: [0, 1],
+      range: [{165, 42, 42}, {70, 130, 180}],
+      interpolate: &Scale.Interpolator.rgb/2
+    )
+
+  stops =
     for i <- 0..4 do
       t = i / 4
-      {t, Scale.map(scale, t)} # => {t, {r,g,b}}
+      # => {t, {r,g,b}}
+      {t, Scale.map(scale, t)}
     end
-  
-   grad =
+
+  grad =
     linear_gradient 40, 40, 420, 420 do
       for {t, {r, g, b}} <- stops do
-        add_stop(t, rgb(r, g, b)) 
+        add_stop(t, rgb(r, g, b))
       end
     end
 
@@ -44,4 +46,3 @@ draw 800, 800 do
   font = load_font("priv/fonts/Alegreya-Regular.otf", 48.0)
   text(font, 80, 215, "Hello, blendend!", fill: rgb(40, 40, 40))
 end
-  
